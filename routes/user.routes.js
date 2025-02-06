583c824c31234a526f95241adb0813cc3ff3c112
aadarsh360 <aadarshpandey64@gmail.com>
2025-02-07 02:03:35 +0530

    use express-validator in register post request

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,16 +1,30 @@
 const express = require("express");
 
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 
-router.get("/register", (req, res)=>{
-    // res.send("user test route");
-    res.render("register")
-})
 
-router.post('/register', (req, res)=>{
-    console.log(req.body);
-    res.send("user registered!!!");
+router.post("/register",
+    body('email').trim().isEmail().isLength({ min: 13 }),
+    body('password').trim().isLength({ min: 5 }),
+    body('username').trim().isLength({ min: 3 }),
+    (req, res) => {
+        // res.send("user test route");
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                message: "Invalid data"
+            })
+        }
+
+        res.send(errors);
+    })
+
+router.get('/register', (req, res) => {
+    res.render('register');
 })
 
 
