5d8c3d337376195043076d06890f6644f83ab460
aadarsh360 <aadarshpandey64@gmail.com>
2025-02-07 00:59:07 +0530

    create user router

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const userRouter = require("./routes/user.routes");
 
 const app = express();
 
-app.get('/', (req, res)=>{
-    res.send("Hello World !!");
-})
+app.set("view engine", "ejs");
+
+app.use('/user', userRouter);
 
 app.listen(3000, ()=>{
     console.log("Server running at port 3000")
