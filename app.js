f5ee22c1b0fe1a98407d600ade126cf1d5a8f248
aadarsh360 <aadarshpandey64@gmail.com>
2025-02-07 11:40:23 +0530

    create login page and token generate use jsonwebtoken and token store in cookie use cookie-parser

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ dotenv.config();
 const connectToDB = require('./config/db');
 connectToDB();
 
+const cookieParser = require('cookie-parser');
+
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,9 +3,14 @@ const express = require("express");
 const router = express.Router();
 const userModel = require('../models/user.model');
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const { body, validationResult } = require('express-validator');
 
+
+router.get('/register', (req, res) => {
+    res.render('register');
+})
 
 
 router.post("/register",
@@ -36,9 +41,60 @@ router.post("/register",
         res.json(newUser);
     })
 
-router.get('/register', (req, res) => {
-    res.render('register');
+
+router.get('/login', (req, res) => {
+    res.render('login');
 })
+
+router.post('/login',
+    body('username').trim().isLength({min:3}),
+    body('password').trim().isLength({min:5}),
+    async (req, res)=>{
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                message: "Invalid data"
+            })
+        }
+
+        const {username, password} = req.body;
+
+        const user = await userModel.findOne({
+            username: username
+        })
+
+        if(!user){
+            return res.status(400).json({
+                message:"username or password is incorrect"
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({
+                message:"username or password is incorrect"
+            })
+        }
+
+        // jsonwebtoken 
+        const token = jwt.sign({
+            userId: user._id,
+            email: user.email,
+            username: user.username,
+        },
+        process.env.JWT_SECRET,
+    )
+
+    res.cookie('token', token);
+
+    res.send("Logged in")
+
+
+    }
+)
+
 
 
 module.exports = router;
