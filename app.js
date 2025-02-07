0fdddebd254a7c4dd25620fa359a7ad168b8f6e8
aadarsh360 <aadarshpandey64@gmail.com>
2025-02-07 13:01:05 +0530

    create home page

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ connectToDB();
 const cookieParser = require('cookie-parser');
 
 const app = express();
+const indexRouter = require('./routes/index.routes');
 
 app.set("view engine", "ejs");
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+app.use('/', indexRouter);
 app.use('/user', userRouter);
 
 
